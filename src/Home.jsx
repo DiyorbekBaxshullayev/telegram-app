0eb2b5f83373c14d63c66b674d00eaad25795830
DiyotbekBaxshullayev <baxshullayevd4@gmail.com>
2025-04-08 11:46:24 +0500

    3

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import CarCard from './components/CarCard';
 
+// Rasm fayllarni import qilish
+import malibuImg from './images/malibu.jpg';
+import k5Img from './images/k5.jpg';
+import gentraImg from './images/gentra.jpg';
+import damasImg from './images/damas.jpg';
+import cobaltImg from './images/cobalt.jpg';
+
+
 const sampleCars = [
   {
     id: 1,
     name: 'Chevrolet Malibu',
     price: 50,
-    image: 'https://cdn.pixabay.com/photo/2016/12/27/21/03/chevrolet-1932300_1280.jpg',
+    image: malibuImg,
   },
   {
     id: 2,
     name: 'Kia K5',
     price: 45,
-    image: 'https://cdn.pixabay.com/photo/2021/03/30/20/54/kia-6140400_1280.jpg',
+    image: k5Img,
   },
   {
     id: 3,
     name: 'Gentra',
     price: 35,
-    image: 'https://uzautomotors.com/uploads/models/gentra_white.png',
+    image: gentraImg,
   },
   {
     id: 4,
     name: 'Damas',
     price: 25,
-    image: 'https://uzautomotors.com/uploads/models/damas_white.png',
+    image: damasImg,
   },
   {
     id: 5,
     name: 'Cobalt',
     price: 30,
-    image: 'https://uzautomotors.com/uploads/models/cobalt_white.png',
+    image: cobaltImg,
   },
 ];
 
